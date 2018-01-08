5ac5486fe81d0aa2af3bdd42922d88db8439bec6
Dawson M <dawson.mortenson@gmail.com>
2018-01-07 21:29:07 -0800

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,51 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Store.destroy_all
+# Store.destroy_all
 Driver.destroy_all
-Message.destroy_all
+# Message.destroy_all
 
-require 'nokogiri'
-require 'httparty'
+# require 'nokogiri'
+# require 'httparty'
+#
+# class Scrapper
+#   attr_accessor :parse_page, :addresses, :store_numbers
+#
+#   def initialize
+#     doc = HTTParty.get("http://drivertimer-beta.herokuapp.com/api/stores/")
+#     @body = JSON.parse(doc.body)
+#     @addresses = []
+#     @store_numbers = []
+#   end
+#
+#   def parse_out_br
+#     parse_page.css('br').each{ |br| br.replace(" ") }
+#   end
+#
+#   def cast_magic
+#     @body.map do |res_obj|
+#       addresses.push(res_obj['address'])
+#       store_numbers.push(res_obj['store_number'])
+#     end
+#   end
+# end
+#
+# scrapper = Scrapper.new
+# scrapper.cast_magic
+#
+# scrapper.store_numbers.each_with_index do |store_number, index|
+#   Store.create!(:store_number => store_number, :address => scrapper.addresses[index], :password => "password")
+# end
 
-class Scrapper
-  attr_accessor :parse_page, :addresses, :store_numbers
+# p "#{Store.all.length} stores created"
 
-  def initialize
-    doc = HTTParty.get("http://drivertimer-beta.herokuapp.com/api/stores/")
-    @body = JSON.parse(doc.body)
-    @addresses = []
-    @store_numbers = []
-  end
+stores = Store.all
 
-  def parse_out_br
-    parse_page.css('br').each{ |br| br.replace(" ") }
-  end
-
-  def cast_magic
-    @body.map do |res_obj|
-      addresses.push(res_obj['address'])
-      store_numbers.push(res_obj['store_number'])
-    end
-  end
+stores.each |store| do
+  store.driver.create!(:name => "Test Driver", :phone_number => "15555555555")
 end
 
-scrapper = Scrapper.new
-scrapper.cast_magic
-
-scrapper.store_numbers.each_with_index do |store_number, index|
-  Store.create!(:store_number => store_number, :address => scrapper.addresses[index], :password => "password")
-end
-
-p "#{Store.all.length} stores created"
-
-home_store = Store.where(:store_number => 177).first
-home_store.drivers.create!(:name => "Dawson", :phone_number => "19715702525")
+# home_store = Store.where(:store_number => 177).first
+# home_store.drivers.create!(:name => "Dawson", :phone_number => "19715702525")
 
 p "#{Driver.all.length} drivers created"
